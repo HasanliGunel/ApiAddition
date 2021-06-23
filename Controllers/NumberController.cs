ab160041a23116d38257d6a286cdbe7c49418fbe
HasanliGunel <hasanligunel@hotmail.com>
2021-06-23 16:15:50 +0400

    first commit

---
+++ Controllers/NumberController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiAddition.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NumberController : ControllerBase
+    {
+        public IActionResult Get(int n)
+        {
+            n = 10;
+            n += 5;
+            return Ok(n);
+        }
+    }
+}
